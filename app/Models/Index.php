384e754edd7117c8ee2d78a3ace5b49f3184bbd6
fielmangginimba <fielmangginimba@gmail.com>
2023-03-04 11:25:23 +0800

    Complete Major MVC Project

--- app/Models/Index.php
+++ app/Models/Index.php
@@ -7,7 +7,7 @@ class Index{
     public static function all(){
         $partial_estate1 = [
             [
-                'head'          =>  'Doral, Florida',
+                'head'          =>  'l, Florida',
                 'body'          =>  '78345',
                 'section'          =>  '204',
                 'description'   =>  'rent | $ 12.000'
